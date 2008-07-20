14409ff21bbe0a22c79912f5ac3d72a0e33d500d
matthew deiters <mdeiters@gmail.com>
2008-07-20 17:39:48 -0500

    cleaning up the transition to rspec

--- test/unit/message_test.rb
+++
@@ -1,9 +0,0 @@
-require '../test_helper'
-
-class MessageTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-   parsed = Date.parse(['2007', '07', '19'].join('/'))
-   assert_nil parsed
-  end
-end
